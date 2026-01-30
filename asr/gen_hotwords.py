a34335f257ce45e4ce9b3b977890eefeb0f70af6
jocelyn <tangxueduo@sihui.email>
2026-01-30 15:26:48 +0800

    [FIX]识别丢失中间结果问题

--- asr/gen_hotwords.py
+++ asr/gen_hotwords.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import json
-import os
 
     
 def convert_mp32wav(mp3_file):
@@ -8,11 +7,15 @@ def convert_mp32wav(mp3_file):
 
 
 def save_hot2json():
-    file = "data/医保中草药数据_共11071条.csv"
+    file = "data/药品频次.csv"
     df = pd.read_csv(file)
     res = []
     count = 0
-    for word in df["名称"].values:
+    # 正序排序
+    print(type(df["usage_count"].values[0]) )
+    df = df.sort_values(by=['usage_count'], ascending=False)
+    print(df["drug_name"])
+    for word in df["drug_name"].values:
         if res and len(res) % 500==0:
             # save
             with open(f"asr/hotwords_{count:02d}.json", "w", encoding="utf-8") as f:
@@ -20,20 +23,23 @@ def save_hot2json():
             res = []
             count +=1
         res.append({"text": word, "weight": 4, "lang": "zh"})
-    print(df["名称"].values)
 
 
 def write_hotwords():
     """"""
     file = "data/医保中草药数据_共11071条.csv"
     df = pd.read_csv(file)
-    print(df["名称"].values)
+    
+    print(f"before unique {len(df['名称'].values)}")
+    df.drop_duplicates(subset=["名称"], inplace=True)
+
+    print(f"after unique {len(df['名称'].values)}")
     with open("asr/hotwords.txt", "w") as fp:
         for word in df["名称"].values:
             fp.write(f"{word}\n")
 
 
 
-if __name__ == "__main__":
-    # write_hotwords()
-    save_hot2json()
+# if __name__ == "__main__":
+#     # write_hotwords()
+#     save_hot2json()
